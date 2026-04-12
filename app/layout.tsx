025a0818929e239af9d3d22d492a1bbc0a3bf39a
narayanasuri <narayanasuri08@gmail.com>
2026-04-12 22:41:44 +0530

    fix: Make app name dynamic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 
 import { ThemeProvider } from "@/components/theme-provider"
@@ -6,6 +7,7 @@ import { cn } from "@/lib/utils"
 
 import "./globals.css"
 import { Navbar } from "@/components/shared/navbar"
+import { APP_NAME } from "@/config"
 
 const geist = Geist({ subsets: ["latin"], variable: "--font-sans" })
 
@@ -13,6 +15,14 @@ const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
 })
+
+export const metadata: Metadata = {
+  title: {
+    default: APP_NAME,
+    template: `%s | ${APP_NAME}`,
+  },
+  description: "",
+}
 
 export default function RootLayout({
   children,
